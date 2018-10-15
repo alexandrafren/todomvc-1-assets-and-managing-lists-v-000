bbba243c3e53ce3df48a8b097d646b43d0b20a98
alexandrafren <alexandra.fren@gmail.com>
2018-10-14 20:27:38 -0700

    site controller

--- to-do/config/routes.rb
+++ to-do/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#the root of the site is routed with "root"
+	root 'site#index'
+
 end
